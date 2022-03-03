289995b452a2639a9c9e811087615213ad58c186
Michael Frieze <michaelfrieze@icloud.com>
2022-03-03 17:56:06 -0500

    imported product styles into ProductsRoute

--- app/routes/products.jsx
+++ app/routes/products.jsx
@@ -1,5 +1,26 @@
 import Products from '~/components/products';
 
+import itemStyles from '~/styles/item.css';
+import priceTagStyles from '~/styles/price-tag.css';
+import titleStyles from '~/styles/title.css';
+
+export const links = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: itemStyles,
+    },
+    {
+      rel: 'stylesheet',
+      href: priceTagStyles,
+    },
+    {
+      rel: 'stylesheet',
+      href: titleStyles,
+    },
+  ];
+};
+
 export default function ProductsRoute() {
   return (
     <div>
