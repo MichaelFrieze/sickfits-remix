7eaa8b1a844a6c84f0f0f67eb9f7258a09ad596d
Michael Frieze <michaelfrieze@icloud.com>
2022-03-04 14:04:29 -0500

    removed api resource routes and used loader function in products route | removed comments

--- app/routes/products.jsx
+++ app/routes/products.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData } from 'remix';
+import { gql } from 'graphql-request';
+import { client } from '~/utils/graphql-client';
 import Products from '~/components/products';
 import productStyles from '~/styles/products.css';
 
@@ -18,7 +20,28 @@ export let meta = () => {
   };
 };
 
-export { loader } from '~/routes/api/products';
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export let loader = async () => {
+  let data = await client.request(ALL_PRODUCTS_QUERY);
+
+  return data;
+};
 
 export default function ProductsRoute() {
   let data = useLoaderData();
@@ -30,36 +53,3 @@ export default function ProductsRoute() {
     </div>
   );
 }
-
-/* 
-Some notes about slow loader function and caching
-
-1. cache the data before accessing it in loader function
-2. or fetch the data on the client side
-
-That's how Remix works. It will call all loaders for the route in parallel. It won't render the new route until all loaders return. This emulates what the browser would do, since the server would need all the data to build the page.
-
-If you loader is slow, then you need to either cache the results on the server, and return that. Or don't call the API from the server, but call it from the client and display a spinner.
-
-Some other types of caching to speed things up:
-you can set cache header to loader too
-export function loader() {
-  return json(data, {
-    headers: {
-      'Cache-Control': 'public, max-age=600'
-    }
-  })
-}
-
-and in the header function
-export const headers({loaderHeaders}) {
-  return {
-    'Cache-Control': loaderHeaders.get('Cache-Control')
-  }
-}
-
-this way the loader (API), and the "page" headers will have the same cache rules 
-
-
-
-*/
