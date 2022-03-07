5a8bd33688d21bbefb84ee0e558fa31c3a340131
Michael Frieze <michaelfrieze@icloud.com>
2022-03-07 14:33:15 -0500

    multiple forms and single button mutations

--- app/components/create-product.jsx
+++ app/components/create-product.jsx
@@ -1,18 +1,47 @@
 import { Form } from 'remix';
 
 export let CreateProduct = () => {
+  let myName = 'Michael Frieze';
   return (
-    <Form method="post">
-      <label htmlFor="name">
-        Name
-        <input type="text" id="name" name="name" placeholder="Name" />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input type="number" id="price" name="price" placeholder="price" />
-      </label>
-
-      <button type="submit">submit</button>
-    </Form>
+    <>
+      <Form method="post">
+        <label htmlFor="product-name">
+          Product Name:{' '}
+          <input
+            type="text"
+            id="product-name"
+            name="productName"
+            placeholder="enter product name"
+          />
+        </label>
+        <br />
+        <label htmlFor="price">
+          Price:{' '}
+          <input
+            type="number"
+            id="price"
+            name="productPrice"
+            placeholder="enter product price"
+          />
+        </label>
+        <br />
+        <button type="submit" name="_action" value="create">
+          create
+        </button>{' '}
+        <button type="submit" name="_action" value="delete">
+          delete
+        </button>{' '}
+        <button type="submit" name="_action" value="error">
+          error
+        </button>
+      </Form>
+      <br />
+      <Form method="post">
+        <input type="hidden" name="myName" value={myName} />
+        <button type="submit" name="_action" value="hidden">
+          hidden
+        </button>
+      </Form>
+    </>
   );
 };
--- app/routes/sell.jsx
+++ app/routes/sell.jsx
@@ -3,14 +3,36 @@ import { useActionData } from 'remix';
 import { CreateProduct } from '~/components/create-product';
 
 export let action = async ({ request }) => {
+  // Multiple Forms and Single Button Mutations
+  // https://www.youtube.com/watch?v=w2i-9cYxSdc&list=PLXoynULbYuEDG2wBFSZ66b85EIspy3fy6&index=5
+
+  // _action is the name of the button and will be equal to button values
+  // For some reason, you cannot use "action" as a name because the browser uses it for other purposes
+  // You can use any other name such as "do" or "perform"
   let formData = await request.formData();
-  let values = Object.fromEntries(formData);
-  return values;
+  let { _action, ...values } = Object.fromEntries(formData);
+
+  if (_action === 'create') {
+    console.log(values);
+    return values;
+  }
+
+  if (_action === 'delete') {
+    let text = 'There is nothing to delete!';
+    console.log(text);
+    return text;
+  }
+
+  if (_action === 'hidden') {
+    console.log(values);
+    return values;
+  }
 };
 
 export default function SellRoute() {
   let actionData = useActionData();
   console.log(actionData);
+
   return (
     <div>
       <CreateProduct />
