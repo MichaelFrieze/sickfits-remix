942cf556654b8127dc6db68d5066b8d960605f7b
Michael Frieze <michaelfrieze@icloud.com>
2022-03-07 22:58:30 -0500

    applied styles to CreateProduct form

--- app/routes/sell.jsx
+++ app/routes/sell.jsx
@@ -1,51 +1,47 @@
-import { useActionData } from 'remix';
+import { useActionData, useLoaderData } from 'remix';
 
-import { CreateProduct } from '~/components/create-product';
+import {
+  CreateProduct,
+  links as createProductStyles,
+} from '~/components/create-product';
+
+export let links = () => {
+  return [...createProductStyles()];
+};
+
+export let loader = () => {
+  let initialValues = {
+    name: 'Nice Shoes',
+    price: 34234,
+    description: 'These are the best shoes!',
+  };
+
+  return initialValues;
+};
 
 export let action = async ({ request }) => {
-  // Multiple Forms and Single Button Mutations
-  // https://www.youtube.com/watch?v=w2i-9cYxSdc&list=PLXoynULbYuEDG2wBFSZ66b85EIspy3fy6&index=5
-
-  // _action is the name of the button and will be equal to button values
-  // For some reason, you cannot use "action" as a name because the browser uses it for other purposes
-  // You can use any other name such as "do" or "perform"
   let formData = await request.formData();
   let { _action, ...values } = Object.fromEntries(formData);
 
-  // slowing the action function down to see pending UI easier on the button
-  await new Promise((res) => {
-    setTimeout(res, 1000);
-  });
+  let reset = 'false';
 
-  if (_action === 'create') {
-    console.log(values);
-    return values;
-  }
-
-  if (_action === 'delete') {
-    let text = 'There is nothing to delete since there is no DB connection!';
-    console.log(text);
-    return text;
-  }
-
-  if (_action === 'hidden') {
-    console.log(values);
-    return values;
-  }
-
-  if (_action === 'addingName') {
-    console.log(values);
-    return values;
+  if (_action === 'reset') {
+    reset = 'true';
+    return {
+      reset,
+    };
   }
 };
 
 export default function SellRoute() {
   let actionData = useActionData();
+  let loaderData = useLoaderData();
+  console.log(loaderData);
   console.log(actionData);
 
   return (
     <div>
-      <CreateProduct />
+      <CreateProduct initialValues={loaderData} />
     </div>
   );
 }
